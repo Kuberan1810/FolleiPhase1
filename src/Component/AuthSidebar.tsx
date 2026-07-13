249892c40c6ab4d8d6ffd5be0d2fe31ec27b1fc6
kuberan <kuberan1810@gmail.com>
2026-07-13 11:29:50 +0530

    fix the deploy error

--- src/Component/AuthSidebar.tsx
+++ src/Component/AuthSidebar.tsx
@@ -1,4 +1,4 @@
-import FolleiWhite from '../assets/logo/Follei-white-new.svg';
+import FolleiWhite from '../assets/logo/follei-white-new.svg';
 import Dashboard from '../assets/login/dashboard1.svg';
 
 interface AuthSidebarProps {
